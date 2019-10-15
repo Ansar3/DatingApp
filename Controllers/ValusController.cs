7a59d1a88d480ac26ce61aca16a6d5716a1b0934
ansar-ali <ansar.ali@analytics.com.pk>
2019-10-15 12:49:16 +0500

    include all changes

---
+++ Controllers/ValusController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValueController(DataContext context)
+        {
+            _context=context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values=await _context.values.ToListAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value=await _context.values.FirstOrDefaultAsync(x=>x.id==id);
+            return Ok(value);
+        }
+    }
+}
